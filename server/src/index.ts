feb7a4e29bbfc7612f3fae0328e8a4154f548653
OfficialGopi <official.gopi@outlook.com>
2025-08-15 11:19:42 +0530

    add express app setup and server initialization; include concurrently dependency

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,20 @@
 import http from "http";
+import { Application } from "express";
+import { env } from "./env";
+import { log } from "./logger";
+import app from "./app";
 
-function main() {}
+function main(app: Application) {
+  const server = http.createServer(app);
+
+  server
+    .listen(env.PORT)
+    .on("listening", () => {
+      log.info(`Server is running on  PORT ${env.PORT}`);
+    })
+    .on("error", (err) => {
+      log.error(err);
+    });
+}
+
+main(app);
