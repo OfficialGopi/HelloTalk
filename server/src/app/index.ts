d056439244d90009071cf2f38402749f21fcc570
OfficialGopi <official.gopi@outlook.com>
2025-08-15 12:01:06 +0530

    add error handling middleware and response utility classes

--- server/src/app/index.ts
+++ server/src/app/index.ts
@@ -1,5 +1,11 @@
 import express from "express";
+import { errorMiddleware } from "./middlewares/error.middlewarre";
 
 const app = express();
+
+//Apis
+
+//Error handling middleware
+app.use(errorMiddleware);
 
 export default app;
