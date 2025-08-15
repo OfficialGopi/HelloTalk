559a59fe025c99dc0279cab576891b4b4d11f888
OfficialGopi <official.gopi@outlook.com>
2025-08-15 11:08:53 +0530

    add environment variable parsing and example configuration

--- src/env.ts
+++ src/env.ts
@@ -3,3 +3,31 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Safeparsing env from process.env via zod
+
+import { z } from "zod";
+
+function envParse(env: NodeJS.ProcessEnv) {
+  const envSchema = z.object({
+    CLIENT_URL: z.string(),
+    CLOUDINARY_API_KEY: z.string(),
+    CLOUDINARY_API_SECRET: z.string(),
+    CLOUDINARY_CLOUD_NAME: z.string(),
+    ACCESS_TOKEN_SECRET: z.string(),
+    REFRESH_TOKEN_SECRET: z.string(),
+    ACCESS_TOKEN_EXPIRY: z.string(),
+    REFRESH_TOKEN_EXPIRY: z.string(),
+    MONGO_URI: z.string(),
+    NODE_ENV: z.string(),
+    PORT: z.coerce.number(),
+  });
+
+  const parsedEnv = envSchema.safeParse(env);
+  if (!parsedEnv.success) {
+    throw new Error(
+      `Invalid environment variables: ${parsedEnv.error.message}`,
+    );
+  }
+  return parsedEnv.data;
+}
+
+export const env = envParse(process.env);
