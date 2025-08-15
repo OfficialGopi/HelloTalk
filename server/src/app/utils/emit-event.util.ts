e98527e52a1390c0c4e900c468903b0143d33ba1
OfficialGopi <official.gopi@outlook.com>
2025-08-15 22:57:33 +0530

    refactor: add type annotation for io in emitEvent function

--- server/src/app/utils/emit-event.util.ts
+++ server/src/app/utils/emit-event.util.ts
@@ -1,9 +1,9 @@
 import { Request } from "express";
 import { getSockets } from "../socket/socket.helper";
-import { userSocketIDs } from "../socket";
+import { Server } from "socket.io";
 
 const emitEvent = (req: Request, event: string, users: string[], data: any) => {
-  const io = req.app.get("io");
+  const io: Server = req.app.get("io");
   const usersSocket = getSockets(users);
   io.to(usersSocket).emit(event, data);
 };
