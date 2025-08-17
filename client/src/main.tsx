c1219a6e0e1df91312842f769ceaead68d56b471
OfficialGopi <official.gopi@outlook.com>
2025-08-17 11:25:42 +0530

    router and tailwindcss initialised

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,3 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Router from "@/router/Router";
 
-createRoot(document.getElementById("root")!).render(<></>);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </>
+);
