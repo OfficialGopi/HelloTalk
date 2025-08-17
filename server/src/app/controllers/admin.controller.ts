36239e1933ed864b7ff672f7841f779eff1d921e
OfficialGopi <official.gopi@outlook.com>
2025-08-17 20:14:11 +0530

    admin login controller added

--- server/src/app/controllers/admin.controller.ts
+++ server/src/app/controllers/admin.controller.ts
@@ -1,7 +1,30 @@
+import { env } from "../../env";
+import { cookieOptions, tokenFieldNames } from "../constants/cookie.constant";
 import { AsyncHandler } from "../utils/async-handler.util";
+import { ApiError, ApiResponse } from "../utils/response-formatter.util";
+import jwt from "jsonwebtoken";
 
 const adminLogin = AsyncHandler(async (req, res, next) => {
-  //TODO
+  const { secretKey } = req.body;
+
+  const isMatched = secretKey === env.ADMIN_SECRET_KEY;
+
+  if (!isMatched) throw new ApiError(401, "Invalid Admin Key");
+
+  const token = jwt.sign(
+    {
+      secretKey: env.ADMIN_SECRET_KEY,
+    },
+    env.ADMIN_TOKEN_SECRET,
+    {
+      expiresIn: env.ADMIN_TOKEN_EXPIRY,
+    } as jwt.SignOptions,
+  );
+
+  return res
+    .status(200)
+    .cookie(tokenFieldNames.adminToken, token, cookieOptions)
+    .json(new ApiResponse(200, {}, "Success"));
 });
 const adminLogout = AsyncHandler(async (req, res, next) => {
   //TODO
