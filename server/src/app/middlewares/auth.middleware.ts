b606ae223c9e20b67cc8e14e0365a4d970a0a70e
OfficialGopi <official.gopi@outlook.com>
2025-08-17 15:59:00 +0530

    Admin Token secret Added

--- server/src/app/middlewares/auth.middleware.ts
+++ server/src/app/middlewares/auth.middleware.ts
@@ -27,7 +27,19 @@ const checkUserIfLoggedIn = AsyncHandler(async (req, res, next) => {
 });
 
 const isAdmin = AsyncHandler(async (req, res, next) => {
-  //TODO
+  const token = req.cookies[tokenFieldNames.adminToken];
+
+  if (!token) throw new ApiError(401, "Only Admin can access this route");
+
+  const secretKey = jwt.verify(token, env.ADMIN_TOKEN_SECRET) as {
+    adminSecretKey: string;
+  } & jwt.JwtPayload;
+
+  const isMatched = secretKey.adminSecretKey === env.ADMIN_SECRET_KEY;
+
+  if (!isMatched) throw new ApiError(401, "Only Admin can access this route");
+
+  next();
 });
 
 export { checkUserIfLoggedIn, isAdmin };
