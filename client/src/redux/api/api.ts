13c9fbd4edf8683a82ed60bccbe4cf931f0b90b0
OfficialGopi <official.gopi@outlook.com>
2025-08-18 10:39:46 +0530

    RTKquery api is added

--- client/src/redux/api/api.ts
+++ client/src/redux/api/api.ts
@@ -1,9 +1,9 @@
+import { SERVER_API_URL } from "@/constants/config";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { server } from "@/constants/config";
 
 const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: `${server}/api/v1/` }),
+  baseQuery: fetchBaseQuery({ baseUrl: SERVER_API_URL }),
   tagTypes: ["Chat", "User", "Message"],
 
   endpoints: (builder) => ({
