5da179878d18729ced57fc79dfb8d4a02ddfe81a
OfficialGopi <official.gopi@outlook.com>
2025-08-18 10:41:48 +0530

    Server URL added to socket

--- client/src/constants/config.ts
+++ client/src/constants/config.ts
@@ -1,1 +1,2 @@
+export const SERVER_URL = `${import.meta.env.VITE_SERVER}`;
 export const SERVER_API_URL = `${import.meta.env.VITE_SERVER}/api/v1`;
--- client/src/lib/Socket.tsx
+++ client/src/lib/Socket.tsx
@@ -1,13 +1,13 @@
 import { createContext, useMemo, useContext } from "react";
 import io, { Socket } from "socket.io-client";
-import { server } from "@/constants/config";
+import { SERVER_URL } from "@/constants/config";
 
 const SocketContext = createContext<Socket | null>(null);
 
 const getSocket = () => useContext(SocketContext);
 
 const SocketProvider = ({ children }: { children: React.ReactNode }) => {
-  const socket = useMemo(() => io(server, { withCredentials: true }), []);
+  const socket = useMemo(() => io(SERVER_URL, { withCredentials: true }), []);
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
