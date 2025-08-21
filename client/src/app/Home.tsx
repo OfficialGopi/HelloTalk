1aebbc2f2df9cef57a25ecfc25b27f438a406739
OfficialGopi <official.gopi@outlook.com>
2025-08-21 09:04:48 +0530

    Home added

--- client/src/app/Home.tsx
+++ client/src/app/Home.tsx
@@ -1,7 +1,13 @@
 import AppLayout from "@/components/layout/AppLayout";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="h-full w-full bg-neutral-100 dark:bg-neutral-950 flex items-center justify-center">
+      <p className="p-8 text-lg font-medium text-center text-neutral-800 dark:text-neutral-200">
+        Select a friend to chat
+      </p>
+    </div>
+  );
 };
 
 export default AppLayout()(Home);
